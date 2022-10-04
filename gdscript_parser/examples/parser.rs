772830f996032d4af3b4b7ffbd4343fa68d5f47a
Isotoxal <62714538+IsotoxalDev@users.noreply.github.com>
2022-10-05 00:28:31 +0530

    Basic example library connection

---
+++ gdscript_parser/examples/parser.rs
@@ -0,0 +1,5 @@
+use gdscript_parser;
+
+fn main() {
+    gdscript_parser::parse("879235293845")
+}
--- gdscript_parser/src/lib.rs
+++ gdscript_parser/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use pest::Parser;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Parser)]
+#[grammar = "gdscript.pest"]
+pub struct GDScriptPestParser;
+
+pub fn parse(what: &str) {
+    let out = GDScriptPestParser::parse(Rule::file, what);
+    println!("{:?}", out);
 }
